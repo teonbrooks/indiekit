0ff89d822c80922f902e319c84f2304948bb8455
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-29 01:45:49 +0100

    feat(frontend): rich text editor

--- packages/frontend/components/textarea/index.js
+++ packages/frontend/components/textarea/index.js
@@ -1,5 +1,62 @@
 import { Controller } from "@hotwired/stimulus";
+import Trix from "trix";
 import { debounce } from "../../lib/utils/debounce.js";
+
+function createStorageKey(file) {
+  const date = new Date();
+  const day = date.toISOString().slice(0, 10);
+  const name = date.getTime() + "-" + file.name;
+
+  return ["tmp", day, name].join("/");
+}
+
+function createFormData(key, file) {
+  const data = new FormData();
+  data.append("key", key);
+  data.append("Content-Type", file.type);
+  data.append("file", file);
+
+  return data;
+}
+
+function uploadFile(file, mediaEndpoint, progressCallback, successCallback) {
+  const key = createStorageKey(file);
+  const formData = createFormData(key, file);
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("POST", mediaEndpoint, true);
+
+  xhr.upload.addEventListener("progress", (event) => {
+    const progress = (event.loaded / event.total) * 100;
+    progressCallback(progress);
+  });
+
+  xhr.addEventListener("load", () => {
+    const alt = "";
+    const url = xhr.getResponseHeader("location");
+    if (xhr.status == 201) {
+      successCallback({ alt, url });
+    }
+  });
+
+  xhr.send(formData);
+}
+
+function uploadFileAttachment(attachment, mediaEndpoint) {
+  if (!attachment.file) {
+    return;
+  }
+
+  uploadFile(attachment.file, mediaEndpoint, setProgress, setAttributes);
+
+  function setProgress(progress) {
+    attachment.setUploadProgress(progress);
+  }
+
+  function setAttributes(attributes) {
+    attachment.setAttributes(attributes);
+  }
+}
 
 export const TextareaController = class extends Controller {
   initialize() {
@@ -16,6 +73,20 @@ export const TextareaController = class extends Controller {
 
     this.element.addEventListener("input", this.adjustHeight);
     window.addEventListener("resize", this.onResize);
+
+    window.addEventListener("trix-before-initialize", () => {
+      Trix.config.blockAttributes.heading1 = {
+        tagName: "h2",
+        terminal: true,
+        breakOnReturn: true,
+        group: false,
+      };
+    });
+
+    window.addEventListener("trix-attachment-add", (event) => {
+      const { mediaEndpoint } = event.target.dataset;
+      uploadFileAttachment(event.attachment, mediaEndpoint);
+    });
   }
 
   disconnect() {
